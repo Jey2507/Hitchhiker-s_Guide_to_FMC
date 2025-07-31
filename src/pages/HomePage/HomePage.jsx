6fe9cae17421fc764d2cedd9b9b4a615793ba063
Oleh Toporovych <otoporovych2@gmail.com>
2025-07-31 20:38:44 +0200

    update site mobile version

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -7,17 +7,15 @@ import imageIce from "../../assets/images/unnamed.png";
 export default function HomePage() {
   return (
     <div className={css.container}>
-      <h2 className={css.heading}>Путівник по кх<br />Для космотуристів</h2>
-      <div className={css.boxWrapped}>
+      <h2 className={css.heading}>Путівник по кх для космотуристів</h2>
         <div className={css.guideContainer}>
           <p className={css.guideIntro}>
           В цьому гайді я спробую пояснити айсберг, яким є збірка на кількість ходів - почати можна дуже просто, але як би довго ти не копав, завжди є щось нове.</p>
-          <p className={css.guideIntro}>Я опишу поетапно збірку фрідріхом<br/><span className={css.spanOne}>Відносно просто збирати саб40</span></p>
-          <p className={css.guideIntro}>Збірку блокбілдингом зі вставками <br/><span className={css.spanOne}>Відносно просто збирати за 29-30</span></p>
-          <p className={css.guideIntro}>Та Збірку др+хтр<br/><span className={css.spanOne}>Метод яким збирають світові топи за 21-22 рухи</span>, надаючи коментарі та ресурси.</p>
+          <p className={css.guideIntro}>Я опишу поетапно збірку фрідріхом, відносно просто збирати саб40</p>
+          <p className={css.guideIntro}>Збірку блокбілдингом зі вставками - відносно просто збирати за 29-30</p>
+          <p className={css.guideIntro}>Та Збірку др+хтр - метод яким збирають світові топи за 21-22 рухи, надаючи коментарі та ресурси</p>
         </div>
         <p className={css.guideIntroOther}>Головне не забувайте, що практикувати вивчене не менш важливо ніж вивчати нові техніки<br/><span className={css.spanTwo}>Саме техніки, бо алгоритмів як ви їх знаєте в кх достатньо мало, і все тримається на техніках та їх розумінні</span>.</p>
-       </div>
       <img className={css.imageIce} src={imageIce} alt="imageIce" />
       <img className={css.imagePlanet} src={imagePlanet} alt="imagePlanet" />
       <img className={css.imageSpace} src={spaceship} alt="spaceship" />
