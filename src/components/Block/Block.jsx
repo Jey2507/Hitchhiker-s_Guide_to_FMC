4d993000e22147fcf2cd5f5368cc6f7c758007ce
Oleh Toporovych <otoporovych2@gmail.com>
2025-10-09 05:54:06 +0200

    update

--- src/components/Block/Block.jsx
+++ src/components/Block/Block.jsx
@@ -10,7 +10,7 @@ export default function Block() {
             <h2 className={css.sectionTitle}>Blockbuilding</h2>
             <p className={css.sectionDescription}>Перше і напевно найпростіше покращення яке можна додати до збірки фрідріхом - використання блоків.<br />Замість того щоб збирати хрест, а потім пари, ви будете збирати “квадрат” 2*2*1, потім додавати його до ребра і отримувати блок 2*2*2 (блок як в x-cross без зібраних 2 елементів хреста), потім додаючи до нього ще один квадрат 2*2*3(блок як в xх-cross без зібраних 1 елементу хреста), і останній квадрат до F2L-1, після чого збірку можна закінчити як зазвичай.</p>
             <h3 className={css.textList}>Ресурси:</h3>
-            <ul className={css.list}>
+            {/* <ul className={css.list}>
                 <li>
                     <p>Блокбілдинг</p>
                 </li>
@@ -39,7 +39,7 @@ export default function Block() {
                 <li className={css.itemVideo}>
                 <YouTubeVideo videoId={"YCuDT4Bfg4s"} color={"orange"}/>
                 </li>
-            </ul>
+            </ul> */}
           </div>
         </>
     )
--- src/pages/BlockPage/BlockPage.jsx
+++ src/pages/BlockPage/BlockPage.jsx
@@ -6,7 +6,6 @@ export default function BlockPage() {
 
   return (
     <div className={css.container}>
-      <h2 className={css.heading}>Блокбілдинг</h2>
       <div className={css.guideContainer}>
         <Block />
       </div>
